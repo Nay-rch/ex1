0b68a81a24820df138d3f4db00c25fdb8e0ea9f9
Nayrch <nayaraluciadarochasilva@gmail.com>
2022-04-05 19:38:42 -0300

    primeiro comentario

---
+++ ex01.java
@@ -0,0 +1,14 @@
+package br.com.generation.www;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Faça um programa que crie um vetor por leitura com 5 valores de pontuação de uma atividade e o escreva em seguida. Encontre após a maior pontuação e a apresente.
+		
+	Scanner leia = new Scanner(System.in)
+			
+
+	}
+
+}
